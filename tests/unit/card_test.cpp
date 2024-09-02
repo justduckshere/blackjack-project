7e2f4be50a2832c150e6641fdc0168db841ab611
justduckshere <pip.cooper@service.nsw.gov.au>
2024-09-02 14:54:02 +1000

    updated test name to incorporate _ between class and fucntion name

--- tests/unit/card_test.cpp
+++ tests/unit/card_test.cpp
@@ -1,89 +1,89 @@
 #include <gtest/gtest.h>
 #include "card.h"
 
-TEST(CardGetSuitShould, ReturnTheSetSuit) {
+TEST(Card_GetSuitShould, ReturnTheSetSuit) {
     Card card("heart", "one");
     EXPECT_EQ(card.getSuit(), "heart");
 }
 
-TEST(CardGetValueShould, ReturnTheSetValue) {
+TEST(Card_GetValueShould, ReturnTheSetValue) {
     Card card("heart", "one");
     EXPECT_EQ(card.getValue(), "one");
 }
 
-TEST(CardSetValueShould, ReturnTheSetValue) {
+TEST(Card_SetValueShould, ReturnTheSetValue) {
     Card card("heart", "one");
     card.setValue("two");
     EXPECT_EQ(card.getValue(), "two");
 }
 
-TEST(CardSetSuitShould, ReturnTheSetSuit) {
+TEST(Card_SetSuitShould, ReturnTheSetSuit) {
     Card card("heart", "one");
     card.setSuit("spade");
     EXPECT_EQ(card.getSuit(), "spade");
 }
 
-TEST(CardGetTrueCardValueShould, ReturnOneForAce){
+TEST(Card_GetTrueCardValueShould, ReturnOneForAce){
     Card card("heart", "ace");
     EXPECT_EQ(card.getCardTrueValue(), 1);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnTwoForTwo){
+TEST(Card_GetTrueCardValueShould, ReturnTwoForTwo){
     Card card("heart", "two");
     EXPECT_EQ(card.getCardTrueValue(), 2);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnThreeForThree){
+TEST(Card_GetTrueCardValueShould, ReturnThreeForThree){
     Card card("heart", "three");
     EXPECT_EQ(card.getCardTrueValue(), 3);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnFourForFour){
+TEST(Card_GetTrueCardValueShould, ReturnFourForFour){
     Card card("heart", "four");
     EXPECT_EQ(card.getCardTrueValue(), 4);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnFiveForFive){
+TEST(Card_GetTrueCardValueShould, ReturnFiveForFive){
     Card card("heart", "five");
     EXPECT_EQ(card.getCardTrueValue(), 5);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnSixForSix){
+TEST(Card_GetTrueCardValueShould, ReturnSixForSix){
     Card card("heart", "six");
     EXPECT_EQ(card.getCardTrueValue(), 6);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnSevenForSeven){
+TEST(Card_GetTrueCardValueShould, ReturnSevenForSeven){
     Card card("heart", "seven");
     EXPECT_EQ(card.getCardTrueValue(), 7);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnEightForEight){
+TEST(Card_GetTrueCardValueShould, ReturnEightForEight){
     Card card("heart", "eight");
     EXPECT_EQ(card.getCardTrueValue(), 8);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnNineForNine){
+TEST(Card_GetTrueCardValueShould, ReturnNineForNine){
     Card card("heart", "nine");
     EXPECT_EQ(card.getCardTrueValue(), 9);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnTenForTen){
+TEST(Card_GetTrueCardValueShould, ReturnTenForTen){
     Card card("heart", "ten");
     EXPECT_EQ(card.getCardTrueValue(), 10);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnTenForJack){
+TEST(Card_GetTrueCardValueShould, ReturnTenForJack){
     Card card("heart", "jack");
     EXPECT_EQ(card.getCardTrueValue(), 10);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnTenForQueen){
+TEST(Card_GetTrueCardValueShould, ReturnTenForQueen){
     Card card("heart", "queen");
     EXPECT_EQ(card.getCardTrueValue(), 10);
 }
 
-TEST(CardGetTrueCardValueShould, ReturnTenForKing){
+TEST(Card_GetTrueCardValueShould, ReturnTenForKing){
     Card card("heart", "king");
     EXPECT_EQ(card.getCardTrueValue(), 10);
 }
