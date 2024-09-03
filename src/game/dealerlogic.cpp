ec23feebc8e094d70bc57bc1efc2fd398307f954
justduckshere <pip.cooper@service.nsw.gov.au>
2024-09-03 10:03:41 +1000

    added comment to make clearer why we only get the total hand making ace equal to 11

--- src/game/dealerlogic.cpp
+++ src/game/dealerlogic.cpp
@@ -14,6 +14,8 @@ void PlayGame::playDealersRound(PlayGameWrapper* wrapper) {
 bool PlayGame::determineIfDealerShouldDraw(PlayGameWrapper* wrapper, bool hasAce){
     int dealersHandTotal = 0;
     if (hasAce == true) {
+        // we only need to call the total for the instance of the ace = 11 as we need to treat the ace
+        // as 11 to verify if it is over 17 
         dealersHandTotal = wrapper->callGetTotalHand(wrapper, wrapper->callGetHand(dealer), 11); 
     }else {
         dealersHandTotal = wrapper->callGetTotalHand(wrapper, wrapper->callGetHand(dealer));
