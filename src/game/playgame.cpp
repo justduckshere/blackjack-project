085f43c8a27e8034aaa726a9f659fd297b7c2c25
justduckshere <pip.cooper@service.nsw.gov.au>
2024-09-03 10:45:19 +1000

    set the highest total for hte dealer after displaying their hand

--- src/game/playgame.cpp
+++ src/game/playgame.cpp
@@ -39,6 +39,8 @@ bool PlayGame::regularGamePlay(PlayGameWrapper *wrapper){
     playPlayersRound(wrapper);
     cout << "\tDealer has ";
     wrapper->callDisplayHand(dealer);
+    setHighestValidHandValueForPlayer(wrapper, dealer);
+
 
     if (wrapper->callCheckIfHandHasGoneBust(wrapper, wrapper->callGetHand(getDealer()))) {
         cout << "Dealer has gone bust! Everyone else has won!!" << endl <<endl;
